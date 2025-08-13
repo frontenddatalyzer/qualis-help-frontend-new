a953d90da562bb6e38c3625112c800cda4c2a357
Mahendra B <mahendrab@datalyzerint.com>
2025-08-13 12:32:40 +0530

    dist folder generated

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,11 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'products/spc/:docId',
+    renderMode: RenderMode.Server  // Render on demand at request time
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
-  }
+  },
 ];
